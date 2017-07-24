bd61ed726931fd18d4e8c88137775be52ed4638f
Vandolf Estrellado <vestrel00@gmail.com>
2017-07-24 08:21:02 -0400

    Created the App, BaseActivity, BaseFragment, and custom PerActivity, PerFragment, and PerChildFragment Scopes. Closes #3

---
+++ app/src/main/java/com/vestrel00/daggerbutterknifemvp/inject/package-info.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2017 Vandolf Estrellado
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Contains the different custom scopes used.
+ * <p>
+ * Note that the standard {@link javax.inject.Singleton} scope is used as the application scope
+ * (there is no PerApplication scope annotation). This is done in order to support singleton
+ * scoped classes from other libraries (which do not have access to this project's custom scopes).
+ */
+package com.vestrel00.daggerbutterknifemvp.inject;
---
+++ app/src/main/java/com/vestrel00/daggerbutterknifemvp/ui/common/BaseActivityModule.java
@@ -0,0 +1,34 @@
+package com.vestrel00.daggerbutterknifemvp.ui.common;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.content.Context;
+
+import com.vestrel00.daggerbutterknifemvp.inject.PerActivity;
+
+import javax.inject.Named;
+
+import dagger.Binds;
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Provides base activity dependencies. This must be included in all activity modules, which must
+ * provide a concrete implementation of {@link Activity}.
+ */
+@Module
+public abstract class BaseActivityModule {
+
+    static final String ACTIVITY_FRAGMENT_MANAGER = "BaseActivityModule.activityFragmentManager";
+
+    @Binds
+    @PerActivity
+    abstract Context activityContext(Activity activity);
+
+    @Provides
+    @Named(ACTIVITY_FRAGMENT_MANAGER)
+    @PerActivity
+    static FragmentManager activityFragmentManager(Activity activity) {
+        return activity.getFragmentManager();
+    }
+}
