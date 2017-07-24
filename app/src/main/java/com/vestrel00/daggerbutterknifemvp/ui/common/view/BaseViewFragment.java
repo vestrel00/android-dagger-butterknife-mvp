304dd241c67442544ede49a64186963e0db22078
Vandolf Estrellado <vestrel00@gmail.com>
2017-07-24 12:24:25 -0400

    Created the MVP framework; presenter and view interface, base activity and fragment, etc. Closes #12

---
+++ app/src/main/java/com/vestrel00/daggerbutterknifemvp/ui/common/view/BaseViewFragment.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2017 Vandolf Estrellado
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.vestrel00.daggerbutterknifemvp.ui.common.view;
+
+import android.os.Bundle;
+import android.support.annotation.CallSuper;
+
+import com.vestrel00.daggerbutterknifemvp.ui.common.presenter.Presenter;
+
+import javax.inject.Inject;
+
+/**
+ * A {@link BaseFragment} that contains and invokes {@link Presenter} lifecycle invocations.
+ *
+ * @param <T> the type of the {@link Presenter}.
+ */
+public abstract class BaseViewFragment<T extends Presenter> extends BaseFragment
+        implements MVPView {
+
+    @Inject
+    protected T presenter;
+
+    @Override
+    public void onViewStateRestored(Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        // Only start the presenter when the views have been bound.
+        // See BaseFragment.onViewStateRestored
+        presenter.onStart(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        presenter.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        presenter.onPause();
+    }
+
+    @CallSuper
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        presenter.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        presenter.onEnd();
+        super.onDestroyView();
+    }
+}
