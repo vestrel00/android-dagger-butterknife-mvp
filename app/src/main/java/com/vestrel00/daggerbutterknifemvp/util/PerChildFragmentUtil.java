97f2d041c3db3a39edc67d00210c53c2ff6f3a90
Vandolf Estrellado <vestrel00@gmail.com>
2017-07-24 09:47:24 -0400

    Create scoped utilities that demonstrate each of the scopes; Singleton, PerActivity, PerFragment, and PerChildFragment. Closes #4

---
+++ app/src/main/java/com/vestrel00/daggerbutterknifemvp/util/PerChildFragmentUtil.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2017 Vandolf Estrellado
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.vestrel00.daggerbutterknifemvp.util;
+
+import android.app.Fragment;
+
+import com.vestrel00.daggerbutterknifemvp.inject.PerChildFragment;
+import com.vestrel00.daggerbutterknifemvp.ui.common.BaseChildFragmentModule;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ * A class that does something.
+ * <p>
+ * This class has the {@link PerChildFragment} scope. This means that the child {@link Fragment}
+ * (a fragment inside a fragment that is added using {@link Fragment#getChildFragmentManager()})
+ * and all of its dependencies will share the same instance of this class.
+ * However, different child fragments instances will have their own instances of this class.
+ * <p>
+ * This is not available at the parent {@link Fragment}, {@link android.app.Activity}
+ * and {@link android.app.Application}.
+ */
+@PerChildFragment
+public final class PerChildFragmentUtil {
+
+    private final Fragment childFragment;
+
+    @Inject
+    PerChildFragmentUtil(@Named(BaseChildFragmentModule.CHILD_FRAGMENT) Fragment childFragment) {
+        this.childFragment = childFragment;
+    }
+
+    /**
+     * @return the result of the work done here as a string. For this example, this returns its
+     * {@link #hashCode()} and the child fragment's {@link #hashCode()}.
+     */
+    public String doSomething() {
+        return "PerChildFragmentUtil: " + hashCode()
+                + ", child Fragment: " + childFragment.hashCode();
+    }
+}
